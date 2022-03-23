2049bfc402d372384c69c31e58505d4a6a31cfc5
scansel <scanseltanzania@gmail.com>
2022-03-23 21:41:43 +0300

    add number base 10

---
+++ 101-clone_repo/number_base_10.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabets in lovercase
+ * followed by new line
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch < 10; ch++)
+	{
+		printf("%d", ch);
+	}
+	printf("\n");
+	return (0);
+}
