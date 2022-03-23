fdcad4b420ea34643f560cdece369200a499adf8
scansel <scanseltanzania@gmail.com>
2022-03-23 20:11:13 +0300

    i was trying to push file

---
+++ 101-clone_repo/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("INSTRUCTIONS\n");
+    printf("To clone repository\n");
+    printf("run the following command in your git bash\n");
+    printf("git clone https://github.com/Account_name/Repository_name.git\n");
+    printf("remote repo will be available to your local machine");
+    return 0;
+}
